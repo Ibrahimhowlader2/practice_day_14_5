9e64d33772fee59c255cf31f928a8537c329efb3
ibrahimhowlader2 <sibrahim.ih2@gmail.com>
2024-03-20 16:39:57 +0600

    First Commit

---
+++ first_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .forms import common_form
+
+# Create your views here.
+
+def home(request):
+    form=common_form()
+    return render(request,'home.html', {"form":form})
